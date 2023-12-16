2694da51cd7fcef3fa3140930c2f2a6bfa3d5bbc
jethro.linley <j.linley.1@research.gla.ac.uk>
2023-12-16 11:08:07 +0000

    rtd

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-i#html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
